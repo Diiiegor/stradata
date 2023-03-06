9be30e703ccd8c4ad1bb930eabf8e0154684d61e
Juan Diego Oliveros <jdoliveros@gtsolutions.io>
2023-03-05 19:06:41 -0500

    Elasticsearch driver

--- app/Repository/ElasticsearchRepository.php
+++ app/Repository/ElasticsearchRepository.php
@@ -3,12 +3,45 @@
 namespace App\Repository;
 
 use App\Contracts\ISearchable;
+use Elasticsearch\ClientBuilder;
 
 class ElasticsearchRepository implements ISearchable
 {
+    private $client;
 
-    public function search(string $name, int $page)
+    public function __construct()
+    {
+        $host = env('ELASTICSEARCH_HOST');
+        $this->client = ClientBuilder::create()->setHosts(["elasticsearch:9200"])->build();
+    }
+
+    public function search(string $name, int $page): array
     {
 
+        $perPage = 10;
+        $offset = ($page - 1) * $perPage;
+        $query = [
+            'index' => 'names',
+            'body' => [
+                'size' => $perPage,
+                'from' => $offset,
+                'sort' => [
+                    '_score' => [
+                        'order' => 'desc'
+                    ]
+                ],
+                'query' => [
+                    'match' => [
+                        'nombre' => [
+                            'query' => $name,
+                            'fuzziness' => 'auto'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $results = $this->client->search($query);
+
+        return array_map(fn($item) => (object)$item['_source'], $results['hits']['hits']);
     }
 }
--- app/Repository/MysqlRepository.php
+++ app/Repository/MysqlRepository.php
@@ -10,8 +10,12 @@ class MysqlRepository implements ISearchable
 
     public function search(string $name, int $page): array
     {
-        $query = People::query()->whereRaw("match(nombre) AGAINST('$name' IN NATURAL LANGUAGE MODE)")
-            ->paginate(10, ['*'], null, $page);
+        $query = People::query()
+            ->select('*')
+            ->selectRaw("MATCH(nombre) AGAINST('$name' IN NATURAL LANGUAGE MODE) AS score")
+            ->whereRaw("match(nombre) AGAINST('$name' IN NATURAL LANGUAGE MODE)")
+            ->orderByDesc('score')
+            ->paginate(10,null, null, $page);
         return $query->items();
     }
 }
